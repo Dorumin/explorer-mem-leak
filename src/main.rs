2a32fe6f166b265a09360d98a79b16d80204376c
Naughty <dorminsama@gmail.com>
2020-12-18 02:34:11 -0300

    Use consts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+// Clippy settings to help my newbie Rust brain
 #![deny(clippy::all)]
 #![deny(clippy::nursery)]
 #![deny(clippy::pedantic)]
+#![allow(clippy::unseparated_literal_suffix)]
+
+// Don't show a command prompt
 #![windows_subsystem = "windows"]
 
 use std::{
@@ -9,6 +13,8 @@ use std::{
     process::Command
 };
 use sysinfo::{Process, ProcessExt, System, SystemExt, Signal};
+
+const MAX_MEMORY_IN_MB: u64 = 500u64;
 
 fn get_explorers(system: &'_ System) -> impl Iterator<Item=&'_ Process> {
     system
@@ -31,7 +37,7 @@ fn restart_process(process: &Process) {
 fn main() {
     loop {
         for process in get_explorers(&System::new_all()) {
-            if process_memory_in_mb(process) > 900 {
+            if process_memory_in_mb(process) > MAX_MEMORY_IN_MB {
                 restart_process(process);
             }
         }
