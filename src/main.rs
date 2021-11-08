716e4442a12485d30da17723e4026288bbc3048c
Dorumin <dorminsama@gmail.com>
2021-11-08 01:42:06 -0300

    Upgrades

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,27 @@
 #![deny(clippy::all)]
 #![deny(clippy::nursery)]
 #![deny(clippy::pedantic)]
-#![allow(clippy::unseparated_literal_suffix)]
 
 // Don't show a command prompt
 #![windows_subsystem = "windows"]
 
-use std::{
-    thread,
-    time::Duration,
-    process::Command
-};
+use std::sync::Arc;
+use std::thread;
+use std::time::{Instant};
+use std::process::Command;
+use std::sync::atomic::{Ordering, AtomicBool};
+use core_extensions::ToTime;
 use sysinfo::{Process, ProcessExt, System, SystemExt, Signal};
 
-const MAX_MEMORY_IN_MB: u64 = 500u64;
+const MAX_MEMORY_IN_MB: u64 = 2000;
+const TIME_BETWEEN_CHECKS: u64 = 60000;
+const CHECK_INTERVAL: u64 = 500;
 
-fn get_explorers(system: &'_ System) -> impl Iterator<Item=&'_ Process> {
+fn find_processes_by_name<'a>(system: &'a System, name: &'a str) -> impl Iterator<Item=&'a Process> {
     system
         .get_processes()
         .values()
-        .filter(|p| p.name() == "explorer.exe")
+        .filter(move |p| p.name() == name)
 }
 
 fn process_memory_in_mb(process: &Process) -> u64 {
@@ -28,20 +30,59 @@ fn process_memory_in_mb(process: &Process) -> u64 {
 }
 
 fn restart_process(process: &Process) {
-    let path = process.exe();
+    let args = process.cmd();
     process.kill(Signal::Kill);
 
-    Command::new(path).spawn().unwrap();
+    let mut process = Command::new(&args[0]);
+    
+    for arg in args.iter().skip(1) {
+        process.arg(arg);
+    }
+
+    process.spawn().unwrap();
+
+    drop(process); // Unlink
 }
 
 fn main() {
-    loop {
-        for process in get_explorers(&System::new_all()) {
-            if process_memory_in_mb(process) > MAX_MEMORY_IN_MB {
-                restart_process(process);
+    let killed = Arc::new(AtomicBool::new(false));
+    let killed2 = Arc::clone(&killed);
+
+    let handle = thread::spawn(move || {
+        let mut last_check = Instant::now();
+
+        loop {
+            if killed2.load(Ordering::SeqCst) {
+                break;
             }
+
+            if last_check.elapsed().as_millis() > TIME_BETWEEN_CHECKS.into() {
+                last_check = Instant::now();
+
+                for process in find_processes_by_name(&System::new_all(), "explorer.exe") {
+                    println!("-> Found {name:?} process using {mem}mb of memory",
+                        name = process.name(),
+                        mem = process_memory_in_mb(process)
+                    );
+
+                    if process_memory_in_mb(process) > MAX_MEMORY_IN_MB {
+                        restart_process(process);
+                    }
+                }
+            }
+    
+            thread::sleep(CHECK_INTERVAL.milliseconds());
+        }
+    });
+
+    ctrlc::set_handler(move || {
+        for process in find_processes_by_name(&System::new_all(), "Taskmgr.exe") {
+            process.kill(Signal::Kill);
         }
 
-        thread::sleep(Duration::from_secs(60 * 30));
-    }
+        killed.store(true, Ordering::SeqCst);
+    })
+    .expect("whoops the Ctrl+C handler failed to be set");
+
+    handle.join().unwrap();
 }
